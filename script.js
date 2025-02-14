74637f16ab82db5f08f4c98477e6d01fcb3e3f05
Machahary07 <jeumachahary07@gmail.com>
2025-02-14 10:48:47 +0530

    Complete script

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ let ul = document.querySelector('ul');
 let input = document.querySelector('input');
 
 btn.addEventListener('click', function() {
-    addItem();
+    let item = document.createElement('li');
+    item.innerText = input.value;
+    
+    let delbtn = document.createElement('button');
+    delbtn.textContent = 'Delete';
+    delbtn.classList.add('delete');
+    item.appendChild(delbtn);
+
+    ul.appendChild(item);
+    input.value = '';
 });
 
-input.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        addItem();
+ul.addEventListener('click', function(event) {
+    if (event.target.nodeName == 'BUTTON') {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("Deleted");
     }
 });
-
-function addItem() {
-    let li = document.createElement('li');
-    li.textContent = input.value;
-    ul.appendChild(li);
-    input.value = '';
-}
