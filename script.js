a48b978aca98ec91d213bd0db7ae86a33b6bddcb
Machahary07 <jeumachahary07@gmail.com>
2025-02-14 11:37:55 +0530

    Added Tailwind for style

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ btn.addEventListener('click', function() {
     
     let delbtn = document.createElement('button');
     delbtn.textContent = 'Delete';
-    delbtn.classList.add('delete');
+    delbtn.classList.add('bg-red-500', 'text-white', 'p-2', 'rounded', 'hover:bg-red-600', 'ml-2');
     item.appendChild(delbtn);
 
     ul.appendChild(item);
